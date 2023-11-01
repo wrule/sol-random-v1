c30ea73fd1d52d6dfb8f656c618e5533d098615f
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-01 15:05:59 +0800

    提交代码

--- contracts/X.sol
+++ contracts/X.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.8.20;
 
 contract X {
   function getRandom() external view returns (uint256) {
-    bytes32 data = keccak256(abi.encodePacked(block.number, msg.sender));
+    bytes32 data = keccak256(abi.encodePacked(
+      block.number,
+      msg.sender
+    ));
     return uint256(data);
   }
 
