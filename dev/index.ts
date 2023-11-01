136fd714a868d45c0dd39e77aebf073994977881
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-01 15:34:53 +0800

    提交代码

--- dev/index.ts
+++ dev/index.ts
@@ -4,12 +4,18 @@ import { deployContract, getContract, getSigner, init, meta } from './utils';
 
 async function main() {
   await meta();
-  // const x = await getContract<X>('X', '0x68B1D87F95878fE05B998F19b66F4baba5De1aed');
-  const x = await deployContract<X>('X');
+  const x = await getContract<X>('X', '0x3Aa5ebB10DC797CAC828524e59A333d0A371443c');
+  // const x = await deployContract<X>('X');
   // const num = await x.getRandom();
   // console.log(num);
   // const a = await getSigner().provider.getBlockNumber();
   // console.log(a);
+  const a = await x.blockNumber();
+  const b = await x.blockTimestamp();
+  console.log(a);
+  console.log(b);
+  const c = await x.getBlockhash(a - 1n);
+  console.log(c);
 }
 
 async function dev() {
