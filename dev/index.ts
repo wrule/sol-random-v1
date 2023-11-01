99f86eb57886b980def9427317f15894627067fa
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-01 15:04:11 +0800

    提交代码

--- dev/index.ts
+++ dev/index.ts
@@ -1,9 +1,10 @@
 import { X } from '../typechain-types';
-import { deployContract, init, meta } from './utils';
+import { deployContract, getContract, init, meta } from './utils';
 
 async function main() {
   await meta();
-  const x = await deployContract<X>('X');
+  const x = await getContract<X>('X', '0x0B306BF915C4d645ff596e518fAf3F9669b97016');
+  // const x = await deployContract<X>('X');
   const num = await x.getRandom();
   console.log(num);
 }
