8ed3fa7fddc1d85acd17f939b9105aa8dd61107e
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-02 15:26:37 +0800

    随机数生成成功

--- dev/index.ts
+++ dev/index.ts
@@ -4,9 +4,31 @@ import { deployContract, getContract, getSigner, init, meta, watchContract } fro
 
 async function main() {
   await meta();
-  // const x = await getContract<X>('X', '0x19d7c1fd38ebe7fc315ede2892ee6d460e7722cf');
-  const x = await deployContract<X>('X');
-  watchContract(x);
+  const x = await getContract<X>('X', '0x3223D582f446AABB4463740BeF4CF5343555cbF3');
+  
+  async function print() {
+    const requestId1 = await x.requestId1();
+    const requestId2 = await x.requestId2();
+    const randomnessSave = await x.randomnessSave();
+    console.log('<------>');
+    console.log(requestId1);
+    console.log(requestId2);
+    console.log(randomnessSave);
+  }
+
+  print();
+
+  const tx = await x.getRandomRequestId();
+  console.log(tx.hash);
+  const tr = await tx.wait();
+  console.log(tr?.hash);
+
+  setInterval(() => {
+    print();
+  }, 5000);
+
+  // const x = await deployContract<X>('X');
+  // watchContract(x);
   // const a = await x.blockNumber();
   // console.log(a);
   // const tx = await x.getRandomRequestId();
