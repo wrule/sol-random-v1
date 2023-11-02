68c91cbd310425f4e686dc378ba4c2e8c2fc1fe0
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-02 12:54:29 +0800

    提交代码

--- contracts/X.sol
+++ contracts/X.sol
@@ -1,19 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBase.sol";
 
-contract X is VRFConsumerBaseV2 {
-  constructor() VRFConsumerBaseV2(0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625) { }
+contract X is VRFConsumerBase {
+  constructor() VRFConsumerBase(
+    0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
+    0x779877A7B0D9E8603169DdbD7836e478b4624789
+  ) { }
 
   uint256 fee = 0.25 * 10 ** 18;
+  bytes32 keyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311;
 
-  function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
-
+  event fulfillRandomnessEvent(bytes32, uint256);
+  function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+    emit fulfillRandomnessEvent(requestId, randomness);
   }
 
-  function getRandomRequestId() external {
-
+  function getRandomRequestId() external returns (bytes32 requestId) {
+    return requestRandomness(keyHash, fee);
   }
 
   function getRandom() external view returns (uint256) {
