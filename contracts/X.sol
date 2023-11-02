3e7ec3a729b2d4d3027ef0fda0513b164772f9c4
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-02 12:16:11 +0800

    提交代码

--- contracts/X.sol
+++ contracts/X.sol
@@ -5,19 +5,15 @@ import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/shared/token/ERC677/LinkToken.sol";
 import "@chainlink/contracts/src/v0.8/vrf/VRFCoordinatorV2.sol";
 
-uint constant a = 1;
 LinkToken constant linkToken = LinkToken(0x779877A7B0D9E8603169DdbD7836e478b4624789);
 VRFCoordinatorV2 constant vrfCoordinatorV2 = VRFCoordinatorV2(0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625);
 
 contract X is VRFConsumerBaseV2 {
-  constructor() VRFConsumerBaseV2(
-    address(vrfCoordinatorV2),
-    address(linkToken)
-  ) { }
+  constructor() VRFConsumerBaseV2(address(vrfCoordinatorV2)) { }
 
   uint256 fee = 0.25 * 10 ** 18;
 
-  function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+  function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
 
   }
 
