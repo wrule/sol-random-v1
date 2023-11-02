56851fc63ba3bdb42f5b3caa096e998a131bfb6d
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-02 15:14:57 +0800

    提交代码

--- dev/index.ts
+++ dev/index.ts
@@ -4,15 +4,15 @@ import { deployContract, getContract, getSigner, init, meta, watchContract } fro
 
 async function main() {
   await meta();
-  const x = await getContract<X>('X', '0x19d7c1fd38ebe7fc315ede2892ee6d460e7722cf');
-  // const x = await deployContract<X>('X');
+  // const x = await getContract<X>('X', '0x19d7c1fd38ebe7fc315ede2892ee6d460e7722cf');
+  const x = await deployContract<X>('X');
   watchContract(x);
   // const a = await x.blockNumber();
   // console.log(a);
   // const tx = await x.getRandomRequestId();
   // console.log(tx.hash);
-  const tx = await await x.sendMessage('jimao hello');
-  console.log(tx.hash);
+  // const tx = await await x.sendMessage('jimao hello');
+  // console.log(tx.hash);
 }
 
 async function dev() {
